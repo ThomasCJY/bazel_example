f0a91e2b687cec9568cc1bedf6ed7fcad06f625c
jchen <jchen@snapchat.com>
2020-08-17 16:06:18 -0700

    example repro

--- android_local_test/src/main/java/com/example/bazel/MainActivity.java
+++ android_local_test/src/main/java/com/example/bazel/MainActivity.java
@@ -14,6 +14,7 @@
 
 package com.example.bazel;
 
+import com.google.common.base.Throwables;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    RuntimeException e = new RuntimeException("as");
+    String s = Throwables.getStackTraceAsString(e);
     setContentView(R.layout.activity_main);
   }
 }
